9e8c9c6717f40b0117adf7d1c1309f19ef98abb4
Ashish Khatri <khatri8.ashish@gmail.com>
2022-05-26 12:01:06 +0530

    Included SafeERC20 and events

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -2,24 +2,29 @@
 pragma solidity >=0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-import "@uniswap/v2-periphery/contracts/interfaces/IWETH.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 
 contract Swap {
-    IERC20 BlazeToken;
-    IUniswapV2Router02 Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
 
+    using SafeERC20 for IERC20;
+    IERC20 BlazeToken;
+
+    IUniswapV2Router02 Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
     address WethContract = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
 
     // Mapping addresses to amount of wei stored in contract.
     mapping(address => uint256) public weiStored;
 
+    event AddLiquidity(address indexed provider, uint256 indexed tokenAmount, uint256 indexed ethAmount);
+    event TokensSwapped(address indexed by, uint256 indexed tokensSupplied, uint256 indexed weiReceived);
+    event WeiClaimed(address indexed by, uint256 indexed weiAmount);
+
     constructor(address _blazeToken) {
         require(
             _blazeToken != address(0),
-            "Swap: Null addres cannot be ERC20 token!"
+            "Swap: Null address cannot be an ERC20 token!"
         );
         BlazeToken = IERC20(_blazeToken);
     }
@@ -38,10 +43,10 @@ contract Swap {
         );
 
         // Transfer BlazeTokens to itself.
-        BlazeToken.transferFrom(msg.sender, address(this), _tokenAmount);
+        BlazeToken.safeTransferFrom(msg.sender, address(this), _tokenAmount);
 
         // Give Router approval for BlazeTokens on its own behalf.
-        BlazeToken.approve(address(Router), _tokenAmount);
+        BlazeToken.safeApprove(address(Router), _tokenAmount);
 
         // Add liquidity to the pool.
         Router.addLiquidityETH{value: msg.value}(
@@ -52,6 +57,8 @@ contract Swap {
             msg.sender,
             block.timestamp + 600
         );
+
+        emit AddLiquidity(msg.sender, _tokenAmount, msg.value);
     }
 
 
@@ -64,10 +71,10 @@ contract Swap {
         );
 
         // Transfer BlazeTokens to itself.
-        BlazeToken.transferFrom(msg.sender, address(this), _tokenAmount);
+        BlazeToken.safeTransferFrom(msg.sender, address(this), _tokenAmount);
 
         // Give Router approval for BlazeTokens on its own behalf.
-        BlazeToken.approve(address(Router), _tokenAmount);
+        BlazeToken.safeApprove(address(Router), _tokenAmount);
 
         // Create a memory address for swap function input argument.
         address[] memory path = new address[](2);
@@ -85,6 +92,8 @@ contract Swap {
 
         // Increases the amount of wei stored in contract for the msg.sender.
         weiStored[msg.sender] += amounts[1];
+
+        emit TokensSwapped(msg.sender, _tokenAmount, amounts[1]);
     }
 
     function withdrawETH(uint256 _weiAmount) external {
@@ -97,5 +106,6 @@ contract Swap {
         weiStored[msg.sender] -= _weiAmount;
 
         payable(msg.sender).transfer(_weiAmount);
+        emit WeiClaimed(msg.sender, _weiAmount);
     }
 }
